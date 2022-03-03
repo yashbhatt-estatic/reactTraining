689b4191d77a269a149b472c81ecb0258300ee83
yash.bhatt <yash.bhatt@estatic-infotech.com>
2022-03-03 18:30:13 +0530

    🎨 Improve formate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
-import Table from './components/Table'
+import Table from './components/parent/table/Table'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Table />
+        <Table></Table>
       </div>
     )
   }
