b4694006661c835a84986f1633c021e1d998ca8e
yash.bhatt <yash.bhatt@estatic-infotech.com>
2022-03-03 13:54:57 +0530

    ✨ features/day1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
-import Apps from './components/App'
+import Table from './Table'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Apps/>
+        <Table />
       </div>
     )
   }
