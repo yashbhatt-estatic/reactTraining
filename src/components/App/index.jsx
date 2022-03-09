286f49b0fbdb2045b7c1ae4a3e54c4642d51d66d
VishalDolera <vishal.dolera@theonetechnologies.co.in>
2022-03-09 12:46:05 +0530

    Revert "Merge branch 'task/day3' into 'master'"
    This reverts commit cadccacc0c43a85096077fcc81999f325190aec5, reversing
    changes made to fa81fd5bb7011b16fde4cf92251a823186c25ee7.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,75 +1,27 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 import User from "../User/user";
-import Obj from "../User/assets/userObj";
-class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { firstName: "" };
-    this.handler = this.handler.bind(this);
-  }
+import Obj from "../User/userObj";
 
-  componentDidMount() {
-    this.setState({ firstName: "Admin" });
-  }
-
-  getSnapshotBeforeUpdate(prevState) {
-    return prevState;
-  }
-
-  componentDidUpdate(prevState, snapshot) {
-    if (snapshot !== null && this.state.firstName !== "Admin") {
-      alert(
-        " User Changed \n previous user : " +
-          snapshot.firstName +
-          "\n Changed to : " +
-          this.state.firstName
-      );
-    }
-  }
-
-  componentWillUnmount() {
-    this.setState({ firstName: "" });
-  }
-
-  handler(id) { 
-
-    const elements = document.getElementsByClassName("activeClass"); 
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].classList.remove('activeClass')
-    }
-
-    const element = document.getElementById(id);
-    element.className = "activeClass";
-
-    var data = Obj.filter(function (item) {
-      return item.id === id;
-    }).map(function ({ firstName }) {
-      return firstName;
-    });
-
-    this.setState({ firstName: data[0] });
-  }
-
+class App extends Component {
   render() {
     return (
       <>
-        <h1>Welcome to the page {this.state.firstName}</h1>
+        <h2>This is Parent class</h2>
         <h1>Table Data</h1>
-        <table>
+        <table className="Table">
           <thead>
             <tr>
-              <th>Id</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>User Name</th>
-              <th>City</th>
-              <th>State</th>
-              <th>Button</th>
+              <th className="Table-Td">Id</th>
+              <th className="Table-Td">First Name</th>
+              <th className="Table-Td">Last Name</th>
+              <th className="Table-Td">Email</th>
+              <th className="Table-Td">User Name</th>
+              <th className="Table-Td">City</th>
+              <th className="Table-Td">State</th>
             </tr>
           </thead>
           <tbody>
-            <User userObj={Obj} handler={this.handler}/>
+            <User userObj={Obj}></User>
           </tbody>
         </table>
       </>
--- src/components/User/user.jsx
+++ src/components/User/user.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
-import "./assets/user.css";
+import React, { Component } from 'react'
+import './user.css'
 
 class User extends Component {
-  
   render() {
     return (
       <>
-        {this.props.userObj.map((Obj) => {
+        {this.props.userObj.map(Obj => {
           return (
-            <tr key={Obj.id} id={Obj.id}>
-              <td>{Obj.id}</td>
-              <td>{Obj.firstName}</td>
-              <td>{Obj.lastName}</td>
-              <td>{Obj.email}</td>
-              <td>{Obj.userName}</td>
-              <td>{Obj.city}</td>
-              <td>{Obj.state}</td>
-              <td>
-                  <button onClick={() => this.props.handler(Obj.id)}>Show</button>
-              </td>
+            <tr>
+              <td className="Table-Td">{Obj.id}</td>
+              <td className="Table-Td">{Obj.firstName}</td>
+              <td className="Table-Td">{Obj.lastName}</td>
+              <td className="Table-Td">{Obj.email}</td>
+              <td className="Table-Td">{Obj.userName}</td>
+              <td className="Table-Td">{Obj.city}</td>
+              <td className="Table-Td">{Obj.state}</td>
             </tr>
-          );
-        })}
+          )
+        }
+        )
+        }
       </>
-    );
+    )
   }
 }
 
-export default User;
+export default User
