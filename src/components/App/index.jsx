bd039f4ac2bf27708392a14b749d482d3b2fd3f5
yash.bhatt <yash.bhatt@estatic-infotech.com>
2022-03-11 18:01:26 +0530

    🎉 task/day6

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,77 +1,15 @@
 import React, { PureComponent } from "react";
 import User from "../User/user";
-import Obj from "../User/assets/userObj";
+import { Container } from "react-bootstrap";
 class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { firstName: "" };
-    this.handler = this.handler.bind(this);
-  }
-
-  componentDidMount() {
-    this.setState({ firstName: "Admin" });
-  }
-
-  getSnapshotBeforeUpdate(prevState) {
-    return prevState;
-  }
-
-  componentDidUpdate(prevState, snapshot) {
-    if (snapshot !== null && this.state.firstName !== "Admin") {
-      alert(
-        " User Changed \n previous user : " +
-          snapshot.firstName +
-          "\n Changed to : " +
-          this.state.firstName
-      );
-    }
-  }
-
-  componentWillUnmount() {
-    this.setState({ firstName: "" });
-  }
-
-  handler(id) { 
-
-    const elements = document.getElementsByClassName("activeClass"); 
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].classList.remove('activeClass')
-    }
-
-    const element = document.getElementById(id);
-    element.className = "activeClass";
-
-    var data = Obj.filter(function (item) {
-      return item.id === id;
-    }).map(function ({ firstName }) {
-      return firstName;
-    });
-
-    this.setState({ firstName: data[0] });
-  }
 
   render() {
     return (
       <>
-        <h1>Welcome to the page {this.state.firstName}</h1>
-        <h1>Table Data</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>User Name</th>
-              <th>City</th>
-              <th>State</th>
-              <th>Button</th>
-            </tr>
-          </thead>
-          <tbody>
-            <User userObj={Obj} handler={this.handler}/>
-          </tbody>
-        </table>
+        <h1 className="text-center">Welcome to the page</h1>
+        <Container className="mt-5 text-dark">
+          <User/>
+        </Container>
       </>
     );
   }
--- src/components/User/user.jsx
+++ src/components/User/user.jsx
@@ -1,27 +1,110 @@
+import { Button, Card, Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import React, { Component } from "react";
 import "./assets/user.css";
 
 class User extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      fetchUser: [],
+      detailsLoaded: false,
+    };
+    this.getUser = this.getUser.bind(this);
+    this.handler = this.handler.bind(this);
+  }
+
+  handler(id) {
+    const users = this.state.users;
+
+    var data = users.data
+      .filter(function (item) {
+        return item.id === id;
+      })
+      .map(function (user) {
+        return user;
+      });
+
+    this.setState({
+      fetchUser: data
+    });
+  }
+
+  componentDidMount() {
+    setTimeout(this.getUser, 1000 * 2);
+  }
+
+  getUser() {
+    fetch("https://reqres.in/api/users?page=2")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          users: data,
+          detailsLoaded: true,
+        });
+      });
+  }
+
   render() {
+    const { users, detailsLoaded, fetchUser } = this.state;
+
     return (
       <>
-        {this.props.userObj.map((Obj) => {
-          return (
-            <tr key={Obj.id} id={Obj.id}>
-              <td>{Obj.id}</td>
-              <td>{Obj.firstName}</td>
-              <td>{Obj.lastName}</td>
-              <td>{Obj.email}</td>
-              <td>{Obj.userName}</td>
-              <td>{Obj.city}</td>
-              <td>{Obj.state}</td>
-              <td>
-                  <button onClick={() => this.props.handler(Obj.id)}>Show</button>
-              </td>
-            </tr>
-          );
-        })}
+        <Container className="bg-light border">
+          {detailsLoaded === false ? (
+            <Row className="justify-content-md-center">
+              <Col xs lg="2">
+                No Data Available
+              </Col>
+            </Row>
+          ) : (
+            users.data.map((Obj) => {
+              return (
+                <Row
+                  md="2"
+                  className="justify-content-md-center border p-3"
+                  key={Obj.id}
+                  id={Obj.id}
+                >
+                  <Col xs lg="4">
+                    {Obj.first_name}
+                  </Col>
+                  <Button
+                    as={Col}
+                    md="2"
+                    variant="dark"
+                    size="md"
+                    onClick={() => this.handler(Obj.id)}
+                  >
+                    Show
+                  </Button>
+                </Row>
+              );
+            })
+          )}
+        </Container>
+        <hr className="mt-5 mb-3 border text-primary border-primary border-5" />
+          {fetchUser.map((data) => {
+            return (
+              <Card className="mt-5" key={data.id}>
+                <Card.Img
+                  variant="top"
+                  className="h-3 w-3"
+                  src={data.avatar}
+                  alt="pic"
+                />
+                <Card.Body>
+                  <Card.Title>User {data.id}</Card.Title>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>First Name :- {data.first_name}</ListGroupItem>
+                  <ListGroupItem>Last Name :- {data.last_name}</ListGroupItem>
+                  <ListGroupItem>Email :- {data.email}</ListGroupItem>
+                </ListGroup>
+                </Card.Body>
+              </Card>
+            );
+          })
+        }
       </>
     );
   }
