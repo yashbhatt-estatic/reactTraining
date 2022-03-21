05e9d03f028e0b6c75588c90eb3a33ad269879bf
yash.bhatt <yash.bhatt@estatic-infotech.com>
2022-03-21 10:28:23 +0530

    🎨 Change footer style

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,8 +1,19 @@
-import { Container } from '@mui/material';
-import './style.css';
+import {
+  AppBar, Container, Toolbar, Typography,
+} from '@mui/material';
 
 function Footer() {
-  return <Container className="footer text-dark bg-white fixed-bottom">Copyright &copy; 2022 </Container>;
+  return (
+    <AppBar position="static" className="bg-light text-dark">
+      <Container maxWidth="md">
+        <Toolbar className="justify-content-center">
+          <Typography variant="body1" color="inherit">
+            Copyright &copy; 2022
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Footer;
