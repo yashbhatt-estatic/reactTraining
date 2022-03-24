40d20f06a6e48125607eb955335437317416d032
Aneri Patel <vishal.dolera@theonetechnologies.co.in>
2022-03-24 12:21:07 +0530

    ✨ add Home button in 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,14 @@
+import { Home } from '@mui/icons-material';
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="p-5">
       <h1>404- Page not Found</h1>
+      <Button type="button" onClick={() => navigate('/')}><Home /></Button>
     </div>
   );
 }
